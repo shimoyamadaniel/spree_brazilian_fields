ab581fd3268ec819ed5136a2fffe5de202088211
Daniel Shimoyama <shimoyama.daniel@gmail.com>
2013-12-01 13:57:25 -0200

    Fix model

--- app/models/spree/personal_information.rb
+++ app/models/spree/personal_information.rb
@@ -1,13 +1,14 @@
 module Spree
   class PersonalInformation < ActiveRecord::Base
 
-      validates :age, :inclusion => { :in => %w(male female) }, :allow_blank => true
+      validates :gender, :inclusion => { :in => %w(male female) }, :allow_blank => true
       validates :full_name, :presence => true, :length => { :maximum => 50 }
       validates :cpf, :uniqueness => true, :length => { :maximum => 50 }
       validates_with CpfValidator
 
-      validates_date :date_of_birth, :before => lambda { 18.years.ago },
-                               :before_message => Spree.t(:must_be_at_least_18_years_old)
+      validates_date :birthday, 
+              :before => lambda { 18.years.ago },
+              :before_message => Spree.t(:must_be_at_least_18_years_old)
 
       belongs_to :user
   
